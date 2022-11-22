5d941ac75ffeee48c44b2172e9f29035d5708a3d
korealu <643949593@qq.com>
2022-11-22 16:57:39 +0800

    添加远程仓库

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="detail-container hide-tab-bar">
+    <van-nav-bar
+      title="房屋详情"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute } from "vue-router";
 
-const route = useRoute()
+const route = useRoute();
 console.log(route.params.id);
 </script>
 
 <style lang="less" scoped>
-
 </style>
